73c2fe12451492fc78e47caf2f95de0db5bcc9e7
harshaks23 <harshaksabbur@gmail.com>
2018-10-20 09:47:01 -0500

    changes

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var path = require('path');
 var app = express()
  
 app.use(express.static('dist'))
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
 app.get('/test', function (req, res) {
   res.send('Hello World')
